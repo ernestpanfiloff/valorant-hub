f043da488d9f7b81b883db9abaa837362f5d2dc7
ernest <ernestas.du@gmail.com>
2023-03-12 22:25:56 +0000

    init

---
+++ agents.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('load', function () {
+    fetch('https://valorant-api.com/v1/agents?isPlayableCharacter=true')
+        .then(response => response.json())
+        .then(response =>
+            displayAgents(response)
+        )
+        .catch(err => console.error(err));
+});
+
+
+function displayAgents(response) {
+    const agents = document.querySelector('.agents');
+    for (let i = 0; i < response.data.length; i++) {
+        agents.innerHTML += `
+            <div class="agents-agent">
+                <div class="agents-agent-content">
+
+                    <span>${response.data[i].role.displayName}</span>
+
+                    <img src="${response.data[i].fullPortraitV2}" alt="${response.data[i].displayName}">
+
+                    <h3>${response.data[i].displayName}</h3>
+                    
+                    <p>${response.data[i].description}</p>
+
+                    <!--- <a href="#" target="_blank">More about ${response.data[i].displayName}</a> --->
+                </div>
+            </div>
+        `;
+    }
+}
