46b5b644f72759664aa6a864434a49fd1e466b91
ernest <ernestas.du@gmail.com>
2023-03-14 16:52:31 +0000

    special titles separated

--- player-titles.js
+++ player-titles.js
@@ -10,9 +10,21 @@ window.addEventListener('load', function () {
 
 
 function displayPlayerTitles(response) {
+    const hiddenTitles = [];
+    for (let i = 0; i < response.data.length; i++) {
+        if (response.data[i].isHiddenIfNotOwned == true) {
+            hiddenTitles.push(response.data[i]);
+        }
+    }
+    console.log(hiddenTitles);
+
+
+
+
     const PlayerTitles = document.querySelector('.PlayerTitles');
     for (let i = 0; i < response.data.length; i++) {
-        PlayerTitles.innerHTML += `
+        if (response.data[i].isHiddenIfNotOwned == false) {
+            PlayerTitles.innerHTML += `
             <div class="PlayerTitles-title">
                 <div class="PlayerTitles-title-content">
 
@@ -22,5 +34,20 @@ function displayPlayerTitles(response) {
                 </div>
             </div>
         `;
+        }
+    }
+
+
+    const HiddenPlayerTitles = document.querySelector('.HiddenPlayerTitles');
+    for (let i = 0; i < hiddenTitles.length; i++) {
+        HiddenPlayerTitles.innerHTML += `
+        <div class="HiddenPlayerTitles-title">
+            <div class="HiddenPlayerTitles-title-content">
+
+                <h3>${hiddenTitles[i].titleText}</h3>
+                
+            </div>
+        </div>
+    `;
     }
 }
