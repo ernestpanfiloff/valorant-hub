a66c12fff1290b5eab97513a4cac457b042b287f
ernest <ernestas.du@gmail.com>
2023-03-14 15:56:51 +0000

    added maps page

---
+++ maps.js
@@ -0,0 +1,32 @@
+
+window.addEventListener('load', function () {
+    fetch('https://valorant-api.com/v1/maps')
+        .then(response => response.json())
+        .then(response =>
+            displayMaps(response)
+        )
+        .catch(err => console.error(err));
+});
+
+
+function displayMaps(response) {
+    const maps = document.querySelector('.maps');
+    for (let i = 0; i < response.data.length; i++) {
+        maps.innerHTML += `
+            <div class="maps-map">
+                <div class="maps-map-content">
+
+                    
+
+                    <img src="${response.data[i].splash}" alt="${response.data[i].displayName}">
+
+                    <h3>${response.data[i].displayName}</h3>
+                    
+                    <p>${response.data[i].coordinates}</p>
+
+                    <!--- <a href="#" target="_blank">More about ${response.data[i].displayName}</a> --->
+                </div>
+            </div>
+        `;
+    }
+}
